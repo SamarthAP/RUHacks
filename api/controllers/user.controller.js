1913d6498eb8ea238753bee14db3ef44704d5911
Colin Chung <colin.d.chung@gmail.com>
2019-05-18 15:59:48 -0400

    abckadlasd

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,15 +9,11 @@ exports.users_get = function (req, res) {
 }
 
 exports.analyse = async function analyse(req, res) {
-    // Creates a client
+
     const client = new vision.ImageAnnotatorClient();
 
-    /**
-     * TODO(developer): Uncomment the following line before running the sample.
-     */
     const fileName = 'assets/test.png';
 
-    // Performs text detection on the local file
     const [result] = await client.textDetection(fileName);
     const detections = result.textAnnotations;
     console.log('Text:');
