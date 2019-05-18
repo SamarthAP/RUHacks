34723a8a4fdf3e1b5515d383d699ae41b02bc510
Colin Chung <colin.d.chung@gmail.com>
2019-05-18 00:43:03 -0400

    base react app

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+export class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>React app</p>
+            </div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Header } from './components/header.js';
+import './index.css';
+
+ReactDOM.render(<Header />, document.getElementById('root'));
