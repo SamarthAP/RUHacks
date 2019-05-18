9998d58dd64605683eb90141da8bfe0561e98259
Samarth Patel <samarth.patel.22@hotmail.com>
2019-05-18 12:08:24 -0400

    Added OCR endpoint

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,8 +1,27 @@
 const User = require('../models/user.model');
+const vision = require('@google-cloud/vision');
 
 exports.users_get = function (req, res) {
     User.find({}, function (err, items) {
         if (err) return next(err);
         res.send(items);
     });
+}
+
+exports.analyse = async function analyse(req, res) {
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    /**
+     * TODO(developer): Uncomment the following line before running the sample.
+     */
+    const fileName = 'assets/test.png';
+
+    // Performs text detection on the local file
+    const [result] = await client.textDetection(fileName);
+    const detections = result.textAnnotations;
+    console.log('Text:');
+    detections.forEach(text => console.log(text));    
+
+    res.send(detections);
 }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const user_controller = require('../controllers/user.controller');
 
 router.get('/all', user_controller.users_get);
+router.get('/analyse', user_controller.analyse);
 
 module.exports = router;
